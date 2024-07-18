5d70fa835fa525f2dd00788d0949a0a4b2d8a639
Hafız <ramizovhafiz@gmail.com>
2024-07-18 09:42:34 +0300

    Generic network layer added

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,15 @@ import 'package:network_example/network/network_service.dart';
 
 import 'home/home_page.dart';
 
+import 'dart:developer' as developer;
+
 void main() async{
   /// Start dio package
   NetworkService.instance.start();
   runApp(const MyApp());
+  developer.log('log me', name: 'my.app.category');
+  developer.log('log me 1', name: 'my.other.category');
+  developer.log('log me 2', name: 'my.other.category');
 }
 
 class MyApp extends StatelessWidget {
